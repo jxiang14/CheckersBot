2cae29774bea3235c51ff0c6c22d029963e943d9
Wen Chen <wc467@cornell.edu>
2025-05-13 14:23:57 -0400

    outlined 3 functions for minimax

---
+++ src/minimax.py
@@ -0,0 +1,7 @@
+
+def minimaxAction(board, depth, player_color):
+    pass
+def minValue(self,gameState,depth, agent, alpha,beta):
+    pass
+def maxValue(self,gameState,depth, agent, alpha,beta):
+    pass
